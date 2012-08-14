45bb09fc881140425217492a52a9544b65bcc56a
Steve - Tait Towers <steve@taittowers.com>
2012-08-14 09:36:35 -0400

    Basic UI and copy functionality working

--- DAMimport/frmMain.Designer.vb
+++ DAMimport/frmMain.Designer.vb
@@ -22,20 +22,27 @@ Partial Class frmMain
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.btnBrowse = New System.Windows.Forms.Button()
+        Me.btnBrowseImages = New System.Windows.Forms.Button()
         Me.lblImages = New System.Windows.Forms.Label()
         Me.lstImages = New System.Windows.Forms.ListBox()
         Me.dlgSelectImages = New System.Windows.Forms.OpenFileDialog()
+        Me.txtShowfilePath = New System.Windows.Forms.TextBox()
+        Me.btnSelectShowfile = New System.Windows.Forms.Button()
+        Me.lblShowfile = New System.Windows.Forms.Label()
+        Me.dlgSelectShowfile = New System.Windows.Forms.FolderBrowserDialog()
+        Me.lblShowName = New System.Windows.Forms.Label()
+        Me.txtShowName = New System.Windows.Forms.TextBox()
+        Me.btnImport = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'btnBrowse
+        'btnBrowseImages
         '
-        Me.btnBrowse.Location = New System.Drawing.Point(131, 12)
-        Me.btnBrowse.Name = "btnBrowse"
-        Me.btnBrowse.Size = New System.Drawing.Size(75, 23)
-        Me.btnBrowse.TabIndex = 1
-        Me.btnBrowse.Text = "Browse"
-        Me.btnBrowse.UseVisualStyleBackColor = True
+        Me.btnBrowseImages.Location = New System.Drawing.Point(131, 12)
+        Me.btnBrowseImages.Name = "btnBrowseImages"
+        Me.btnBrowseImages.Size = New System.Drawing.Size(75, 23)
+        Me.btnBrowseImages.TabIndex = 1
+        Me.btnBrowseImages.Text = "Browse"
+        Me.btnBrowseImages.UseVisualStyleBackColor = True
         '
         'lblImages
         '
@@ -49,9 +56,11 @@ Partial Class frmMain
         'lstImages
         '
         Me.lstImages.FormattingEnabled = True
+        Me.lstImages.HorizontalScrollbar = True
         Me.lstImages.Location = New System.Drawing.Point(12, 41)
         Me.lstImages.Name = "lstImages"
-        Me.lstImages.Size = New System.Drawing.Size(194, 212)
+        Me.lstImages.ScrollAlwaysVisible = True
+        Me.lstImages.Size = New System.Drawing.Size(406, 212)
         Me.lstImages.TabIndex = 3
         '
         'dlgSelectImages
@@ -61,23 +70,90 @@ Partial Class frmMain
         Me.dlgSelectImages.InitialDirectory = "Environment.GetFolderPath(Environment.SpecialFolder.Personal)"
         Me.dlgSelectImages.Multiselect = True
         '
+        'txtShowfilePath
+        '
+        Me.txtShowfilePath.Location = New System.Drawing.Point(83, 259)
+        Me.txtShowfilePath.Name = "txtShowfilePath"
+        Me.txtShowfilePath.Size = New System.Drawing.Size(254, 20)
+        Me.txtShowfilePath.TabIndex = 4
+        '
+        'btnSelectShowfile
+        '
+        Me.btnSelectShowfile.Location = New System.Drawing.Point(343, 256)
+        Me.btnSelectShowfile.Name = "btnSelectShowfile"
+        Me.btnSelectShowfile.Size = New System.Drawing.Size(75, 23)
+        Me.btnSelectShowfile.TabIndex = 5
+        Me.btnSelectShowfile.Text = "Browse"
+        Me.btnSelectShowfile.UseVisualStyleBackColor = True
+        '
+        'lblShowfile
+        '
+        Me.lblShowfile.AutoSize = True
+        Me.lblShowfile.Location = New System.Drawing.Point(9, 262)
+        Me.lblShowfile.Name = "lblShowfile"
+        Me.lblShowfile.Size = New System.Drawing.Size(47, 13)
+        Me.lblShowfile.TabIndex = 6
+        Me.lblShowfile.Text = "Showfile"
+        '
+        'dlgSelectShowfile
+        '
+        Me.dlgSelectShowfile.SelectedPath = "\\fs01\showfile"
+        '
+        'lblShowName
+        '
+        Me.lblShowName.AutoSize = True
+        Me.lblShowName.Location = New System.Drawing.Point(9, 288)
+        Me.lblShowName.Name = "lblShowName"
+        Me.lblShowName.Size = New System.Drawing.Size(65, 13)
+        Me.lblShowName.TabIndex = 7
+        Me.lblShowName.Text = "Show Name"
+        '
+        'txtShowName
+        '
+        Me.txtShowName.Location = New System.Drawing.Point(83, 285)
+        Me.txtShowName.Name = "txtShowName"
+        Me.txtShowName.Size = New System.Drawing.Size(335, 20)
+        Me.txtShowName.TabIndex = 8
+        '
+        'btnImport
+        '
+        Me.btnImport.Location = New System.Drawing.Point(12, 322)
+        Me.btnImport.Name = "btnImport"
+        Me.btnImport.Size = New System.Drawing.Size(405, 45)
+        Me.btnImport.TabIndex = 9
+        Me.btnImport.Text = "Import Images"
+        Me.btnImport.UseVisualStyleBackColor = True
+        '
         'frmMain
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(430, 262)
+        Me.ClientSize = New System.Drawing.Size(430, 379)
+        Me.Controls.Add(Me.btnImport)
+        Me.Controls.Add(Me.txtShowName)
+        Me.Controls.Add(Me.lblShowName)
+        Me.Controls.Add(Me.lblShowfile)
+        Me.Controls.Add(Me.btnSelectShowfile)
+        Me.Controls.Add(Me.txtShowfilePath)
         Me.Controls.Add(Me.lstImages)
         Me.Controls.Add(Me.lblImages)
-        Me.Controls.Add(Me.btnBrowse)
+        Me.Controls.Add(Me.btnBrowseImages)
         Me.Name = "frmMain"
         Me.Text = "Image Import"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents btnBrowse As System.Windows.Forms.Button
+    Friend WithEvents btnBrowseImages As System.Windows.Forms.Button
     Friend WithEvents lblImages As System.Windows.Forms.Label
     Friend WithEvents lstImages As System.Windows.Forms.ListBox
     Friend WithEvents dlgSelectImages As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents txtShowfilePath As System.Windows.Forms.TextBox
+    Friend WithEvents btnSelectShowfile As System.Windows.Forms.Button
+    Friend WithEvents lblShowfile As System.Windows.Forms.Label
+    Friend WithEvents dlgSelectShowfile As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents lblShowName As System.Windows.Forms.Label
+    Friend WithEvents txtShowName As System.Windows.Forms.TextBox
+    Friend WithEvents btnImport As System.Windows.Forms.Button
 
 End Class
--- DAMimport/frmMain.vb
+++ DAMimport/frmMain.vb
@@ -4,12 +4,39 @@
 
     End Sub
 
-    Private Sub btnBrowse_Click(sender As System.Object, e As System.EventArgs) Handles btnBrowse.Click
+    Private Sub btnBrowseImages_Click(sender As System.Object, e As System.EventArgs) Handles btnBrowseImages.Click
         dlgSelectImages.ShowDialog()
     End Sub
 
     Private Sub dlgSelectImages_FileOk(sender As System.Object, e As System.ComponentModel.CancelEventArgs) Handles dlgSelectImages.FileOk
         lstImages.Items.Clear()
         lstImages.Items.AddRange(dlgSelectImages.FileNames)
+    End Sub
+
+
+
+    Private Sub btnSelectShowfile_Click(sender As System.Object, e As System.EventArgs) Handles btnSelectShowfile.Click
+        Dim showfileResult As DialogResult = dlgSelectShowfile.ShowDialog()
+
+        If (showfileResult = Windows.Forms.DialogResult.OK) Then
+            txtShowfilePath.Text = dlgSelectShowfile.SelectedPath
+        End If
+    End Sub
+
+    
+    Private Sub btnImport_Click(sender As System.Object, e As System.EventArgs) Handles btnImport.Click
+        If (txtShowfilePath.Text.Length > 0 And lstImages.Items.Count > 0) Then
+            For Each imagePath As String In lstImages.Items
+                If (System.IO.File.Exists(imagePath) = True) Then
+                    Dim imageName As String = System.IO.Path.GetFileName(imagePath)
+                    Dim newImageName As String = txtShowName.Text & "-" & imageName
+                    Dim newImagePath As String = txtShowfilePath.Text & newImageName
+
+                    System.IO.File.Copy(imagePath, newImagePath)
+                End If
+            Next
+        Else
+            MessageBox.Show("Please select images and a show name before importing")
+        End If
     End Sub
 End Class
